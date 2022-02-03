d74547314209dcd63bebf105605440fc39b79b41
aviranjan07 <ad3447578@gmail.com>
2022-02-03 10:06:08 +0530

    build cash amount functions

---
+++ app.js
@@ -0,0 +1,24 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message")
+
+checkButton.addEventListener("click", function validCashBillAndCashAmount () {
+    message.getElementsByClassName.display = "none";
+    if(billAmount.value > 0 ){
+        if(cashGiven.value >= billAmount.value){
+
+        }else {
+            showMessage(
+                "The Cash Provided Should Atleast Be Equal To The Bill Amount"
+            );
+        }
+    }else{
+        showMessage = ("Invalid Bill Amount"); 
+    }
+});
+
+function showMessage (message) {
+    message.style.display = "block";
+    message.innerText = message;
+}
