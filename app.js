84232ef1e8226e1527faa58e70bb545f968a8db4
aviranjan07 <ad3447578@gmail.com>
2022-02-03 10:58:26 +0530

    added calculate functions

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message")
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validCashBillAndCashAmount () {
     hideMessage();
@@ -19,9 +22,16 @@ checkButton.addEventListener("click", function validCashBillAndCashAmount () {
     }
 });
 
-// function calculateChange() {
+function calculateChange(amountToBeReturned) {
+    for(let i = 0; i < availableNotes.length; i++){
+         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
 
-// }
+       amountToBeReturned %= availableNotes[i];
+       noOfNotes[i].innerText = numberOfNotes;
+    }   
+}
+
+
 
 function hideMessage() {
     message.style.display = "none";
