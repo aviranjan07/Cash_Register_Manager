83d593ad6d1091b3756525ffd73bd0f1c09cfa67
aviranjan07 <ad3447578@gmail.com>
2022-02-03 13:17:53 +0530

    build by me all things

--- app.js
+++ app.js
@@ -1,43 +1,68 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+const notesAccepted = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
+const form = document.querySelector(".user-input");
+const bill = document.querySelector(".bill-amount");
+const cash = document.querySelector(".cash-amount");
+const error = document.querySelector(".error");
+const noteList = document.querySelector(".note-list");
+const result = document.querySelector(".result-display");
+const root = document.querySelector(":root");
 
-const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+let darkMode = true;
 
-checkButton.addEventListener("click", function validCashBillAndCashAmount () {
-    hideMessage();
-    if(billAmount.value > 0 ){
-        if(cashGiven.value >= billAmount.value){
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-        }else {
-            showMessage(
-                "The Cash Provided Should Atleast Be Equal To The Bill Amount"
-            );
-        }
-    }else{
-        showMessage = ("Invalid Bill Amount"); 
-    }
+result.style.display = "none";
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  result.style.display = "";
+  if (Number(cash.value) < Number(bill.value)) {
+    noteList.style.display = "none";
+    error.textContent = "Cash paid is less than the bill amount!";
+  } else if (Number(cash.value) === Number(bill.value)) {
+    noteList.style.display = "none";
+    error.textContent = "Yayyy! You paid the exact bill amount!";
+  } else {
+    noteList.style.display = "";
+    error.textContent =
+      "Amount To Collect: " +
+      (Number(cash.value) - Number(bill.value)) +
+      "Rupees";
+    const notes = cashRegister(
+      notesAccepted,
+      Number(bill.value),
+      Number(cash.value)
+    );
+    noteList.innerHTML = Object.keys(notes)
+      .map((note) => {
+        return (
+          "<li>" +
+          "<span>" +
+          note +
+          "</span>" +
+          "-" +
+          "<span>" +
+          notes[note] +
+          "</span>" +
+          "</li>"
+        );
+      })
+      .join("");
+  }
+  bill.value = "";
+  cash.value = "";
 });
 
-function calculateChange(amountToBeReturned) {
-    for(let i = 0; i < availableNotes.length; i++){
-         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+function cashRegister(notesAccepted, billAmount, cashPaid) {
+  const cashToReturn = cashPaid - billAmount;
+  const notes = {};
+  let bill = cashToReturn;
 
-       amountToBeReturned %= availableNotes[i];
-       noOfNotes[i].innerText = numberOfNotes;
-    }   
-}
-
-
-
-function hideMessage() {
-    message.style.display = "none";
-}
-
-function showMessage (msg) {
-    message.style.display = "block";
-    message.innerText = msg;
+  for (let i = 0; i < notesAccepted.length; i++) {
+    if (bill % notesAccepted[i] === billAmount) {
+      notes[notesAccepted[i]] = 0;
+    } else {
+      notes[notesAccepted[i]] = Math.floor(bill / notesAccepted[i]);
+      bill = bill % notesAccepted[i];
+    }
+  }
+  return notes;
 }
